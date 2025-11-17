47251a7b5b3aefacb142e1d011c3310bef62441c
Ignacio Antequera Sanchez <ignacioantequera2001@gmail.com>
2025-11-17 11:00:05 +0100

    Enhance object tracking by converting goalkeepers to players and organizing tracks for players, referees, and the ball.

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -20,14 +20,48 @@ class Tracker:
         
         detections = self.detect_frames(frames)
         
+        tracks = {
+            "players": [],
+            "referees": [],
+            "ball": []
+        }
+        
         for frame_num, detection in enumerate(detections):
             cls_names = detection.names
             cls_names_inv = {v: k for k, v in cls_names.items()}
-            print(cls_names)
             
             # Convert the detection boxes to supervision format
             detection_supervision = sv.Detections.from_ultralytics(detection)
             
-            print(detection_supervision)
+            # Convert goalkeeper to player object
+            for object_ind, class_id in enumerate(detection_supervision.class_id):
+                if cls_names_inv[class_id] == "goalkeeper":
+                    detection_supervision.class_id[object_ind] = cls_names_inv.index("player")
             
-            break
+            # Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+            
+            tracks["players"].append({})
+            tracks["referees"].append({})
+            tracks["ball"].append({})
+            
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+                
+                if cls_id == cls_names_inv["player"]:
+                    tracks["players"][frame_num][track_id] = {"bbox": bbox}
+                if cls_id == cls_names_inv["referee"]:
+                    tracks["referees"][frame_num][track_id] = {"bbox": bbox}
+            
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                
+                if cls_id == cls_names_inv["ball"]:
+                    tracks["ball"][frame_num][1] = {"bbox": bbox}
+            
+            print(detection_with_tracks)
+        
+        return tracks
