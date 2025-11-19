9a9e0b16f854a21e9fe433a11db853c24b82e112
Ignacio Antequera Sanchez <ignacioantequera2001@gmail.com>
2025-11-19 12:25:16 +0100

    Add team assignment logic to main function and update player tracks with team information

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import cv2 as cv
+from team_assigner import TeamAssigner
 
 def main():
     # Read video frames
@@ -13,10 +14,21 @@ def main():
                                        read_from_stub = True,
                                        stub_path = "stubs/track_stubs.pkl")
     
+    # Assign teams to players
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks)
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+            tracks['players'][frame_num][player_id]["team"] = team
+            tracks['players'][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+            
+    
     # Draw annotations on video frames
     
     ## Draw object tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    output_video_frames = tracker.draw_annotations(video_frames, tracks['players'][0])
     
     # Save video frames
     save_video(output_video_frames, "output_videos/Take_N1_output.avi", fps=fps)
