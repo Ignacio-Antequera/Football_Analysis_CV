67219a5713ed045b3befa01ec50876eb6b894c9c
Ignacio Antequera Sanchez <ignacioantequera2001@gmail.com>
2025-11-19 12:37:28 +0100

    Update player ellipse drawing to use team-specific colors

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -148,7 +148,8 @@ class Tracker:
             
             # Draw players
             for track_id, player in player_dict.items():
-                frame = self.draw_elipse(frame, player["bbox"], (0, 0, 255))
+                color = player.get("team_color", (0, 0, 255))
+                frame = self.draw_elipse(frame, player["bbox"], color)
             
             # Draw referees
             for _, referee in referee_dict.items():
